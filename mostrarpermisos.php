a56f7e20db9ac065127bafd2f0423fac21f90440
afzamarras <afzamarras@unal.edu.co>
2020-02-17 19:23:30 -0500

    Update mostrarpermisos.php

--- mostrarpermisos.php
+++ mostrarpermisos.php
@@ -69,6 +69,60 @@ $info .= (($permisos & 0x0001) ?
 
 echo $info;
 ?>
+        
+<head>
+	<title>Permisos</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1000">
+
+</head>
+<style>
+		table {
+			width: 80%;
+			height: 600px;
+            background-color: gray;
+            position:absolute;
+            top: 100px;
+            left: 200px;
+			}
+			th, td {
+			width: 30%;
+			}
+</style>
+<body>
+	<table data-vertable="ver1" style="font-size:100px" border="20px">
+		<thead>
+			<tr class="row100 head">
+				<th class="column100 column1" data-column="column1"></th>
+				<th class="column100 column2" data-column="column2">Usuario</th>
+				<th class="column100 column3" data-column="column3">Grupo</th>
+				<th class="column100 column4" data-column="column4">Otros</th>
+
+			</tr>
+		</thead>
+		<tbody>
+			<tr class="row100">
+				<td class="column100 column1" data-column="column1">r</td>
+				<td class="column100 column2" data-column="column2"></td>
+				<td class="column100 column3" data-column="column3"></td>
+				<td class="column100 column4" data-column="column4"></td>
+			</tr>
+			<tr class="row100">
+				<td class="column100 column1" data-column="column1">w</td>
+				<td class="column100 column2" data-column="column2"></td>
+				<td class="column100 column3" data-column="column3"></td>
+				<td class="column100 column4" data-column="column4"></td>
+			</tr>
+			<tr class="row100">
+				<td class="column100 column1" data-column="column1">x</td>
+				<td class="column100 column2" data-column="column2"></td>
+				<td class="column100 column3" data-column="column3"></td>
+				<td class="column100 column4" data-column="column4"></td>
+			</tr>							
+			
+		</tbody>
+	</table>
+</body>
 <div>
 <a href="index.php?rutac=<?php echo $_GET[rutac]?>">atras</a><a href="index.php?rutac="> raiz</a>
     </header>
